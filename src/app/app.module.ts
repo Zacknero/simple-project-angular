6ddd1dddf0c08e7e7cf0f2d391193e8dd38df1d0
Zacknero <nerodevilman@live.it>
2018-07-12 09:20:35 +0200

    new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpServicesService} from './http-services.service';
+import {LoginComponent} from './login/login.component';
+import {TreeDomListComponent} from './tree-dom-list/tree-dom-list.component';
+import {FilterTreePipe} from './filter-tree.pipe';
+
+const appRoutes: Routes = [
+  {
+    path: 'treeDomList',
+    component: TreeDomListComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TreeDomListComponent,
+    FilterTreePipe
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [HttpServicesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
